7935dfe570ef083b475d0a04c053e9e40d3d65c6
shicy <shicy85@163.com>
2019-01-31 14:33:13 +0800

    输入对话框

--- src/components/dialog/InputDialog.js
+++ src/components/dialog/InputDialog.js
@@ -1,23 +1,69 @@
 // components/dialog/InputDialog.js
 Component({
-  /**
-   * 组件的属性列表
-   */
+
   properties: {
-
+    show: {
+      type: "Boolean",
+      value: false,
+      observer: function (newval) {
+        if (newval) {
+          this.setData({ focus: true });
+        }
+      }
+    },
+    title: {
+      type: "String",
+      value: "标题",
+      observer: function (newval, oldval) {
+      }
+    },
+    label: {
+      type: "String",
+      value: ""
+    },
+    placeholder: {
+      type: "String",
+      value: "点击输入"
+    },
+    value: {
+      type: "String",
+      value: "",
+      observer: function (newval) {
+        this.setData({ text: newval || "" });
+      }
+    },
+    showAsPassword: {
+      type: "Boolean",
+      value: false
+    }
   },
 
-  /**
-   * 组件的初始数据
-   */
   data: {
-
+    text: "",
+    focus: false
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  lifetimes: {
+    attached () {
+      this.setData({ text: this.data.value });
+    }
+  },
 
+  methods: {
+    onInputHandler (e) {
+      this.setData({ text: e.detail.value });
+    },
+
+    onSubmitHandler () {
+      let myEventDetail = { value: this.data.text };
+      this.triggerEvent("submit", myEventDetail);
+      if (!myEventDetail.preventDefault)
+        this.setData({ show: false });
+    },
+
+    onCancelHandler () {
+      this.triggerEvent("cancel");
+      this.setData({ show: false });
+    }
   }
 })
