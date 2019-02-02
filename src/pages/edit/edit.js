72649dac35219722f0afc268e82e2e387ab4d1af
shicy <shicy@manfenjiayuan.com>
2019-02-03 00:05:23 +0800

    编辑页面

--- src/pages/edit/edit.js
+++ src/pages/edit/edit.js
@@ -15,16 +15,22 @@ Page({
   onLoad: function (options) {
     this.currentItem = wx.getStorageSync("data_for_edit") || {};
     wx.removeStorageSync("data_for_edit");
-    console.log(this.currentItem);
 
     let _data = { title: "", content: "" };
-    if (this.currentItem.title) {
-      _data.title = Crypto.decrypt(app.userSecret, this.currentItem.title);
+    if (this.currentItem && this.currentItem._id) {
+      app.getSecret(true).then((secret) => {
+        if (this.currentItem.title) {
+          _data.title = Crypto.decrypt(secret, this.currentItem.title);
+        }
+        if (this.currentItem.content) {
+          _data.content = Crypto.decrypt(secret, this.currentItem.content);
+        }
+        this.setData(_data);
+      });
     }
-    if (this.currentItem.content) {
-      _data.content = Crypto.decrypt(app.userSecret, this.currentItem.content);
+    else {
+      this.setData(_data);
     }
-    this.setData(_data);
   },
 
   onTitleInputHandler (e) {
@@ -45,22 +51,24 @@ Page({
     if (!params.content)
       return app.$tooltip("请输入内容");
     
-    if (this.currentItem._id)
-      params._id = this.currentItem._id;
-    params.groupId = this.currentItem.groupId;
-    params.title = Crypto.encrypt(app.userSecret, params.title);
-    params.content = Crypto.encrypt(app.userSecret, params.content);
-    
-    wx.showLoading({ title: "正在提交...", mask: true });
-    app.cloudFunction("saveItem", params, (err, ret) => {
-      wx.hideLoading();
-      if (!err) {
-        wx.showToast({ title: "保存成功", duration: 1500 });
-        wx.setStorageSync("page_edit_saved", "1");
-        setTimeout(() => {
-          wx.navigateBack({});
-        }, 1500);
-      }
+    app.getSecret(true).then((secret) => {
+      if (this.currentItem._id)
+        params._id = this.currentItem._id;
+      params.groupId = this.currentItem.groupId;
+      params.title = Crypto.encrypt(secret, params.title);
+      params.content = Crypto.encrypt(secret, params.content);
+
+      wx.showLoading({ title: "正在提交...", mask: true });
+      app.cloudFunction("saveItem", params, (err, ret) => {
+        wx.hideLoading();
+        if (!err) {
+          wx.showToast({ title: "保存成功", duration: 1500 });
+          wx.setStorageSync("page_edit_saved", "1");
+          setTimeout(() => {
+            wx.navigateBack({});
+          }, 1500);
+        }
+      });
     });
   }
 
